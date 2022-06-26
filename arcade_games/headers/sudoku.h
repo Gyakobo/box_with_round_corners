67ad9cd770c3f4db27d9fe96c7b532ad2e487ce5
root <andygyakobo@gmail.com>
2022-06-26 10:40:06 -0400

    Gave up, and just started referring straight to mfgravesjr github

--- arcade_games/headers/sudoku.h
+++ arcade_games/headers/sudoku.h
@@ -50,4 +50,89 @@ public:
 	void set_problem();
 };
 
+namespace sudoku {
+
+	#define boxOrigin 0
+	#define rowOrigin 0
+	#define colOrigin 0
+
+	// per box formula, basically needed for the shuffling of numbers in the grid
+	// this formula goes through each box instead of the natural order
+	// ((i / 3) % 3) * 9 + ((i % 27) / 9) * 3 + (i / 27) * 27 + (i %3)
+	int formula(int i) {
+		return ((i / 3) % 3) * 9 + ((i % 27) / 9) * 3 + (i / 27) * 27 + (i %3);
+	}
+
+	// get box origin formula
+	// this formula gives the index of the origin of the box that contains index i(0-80)
+	// (i % 9) / 3) * 3 + (i / 27) * 27 
+	int get_box_origin(int i) {
+		return ((i % 9) / 3) * 3 + (i / 27) * 27;
+	} 
+
+	// get row origin formula 
+	// this formula gives the index of the origin of the row that contains index i(0-80)
+	int get_row_origin(int i) {
+		return (i/9) * 9;
+	}
+
+	// get column origin formula
+	// this formula gives the index of the origin of the column that contains index i(0-80)
+	int get_column_origin(int i) {
+		return (i%9);
+	}
+
+	// get box origin formula 
+	// this formula gives the index of origin of box # i (0-8)
+	int get_box_origin_2(int i) {
+		return (i * 3) % 9 + ((i * 3) / 9) * 27;
+	}
+
+	// get row origin formula
+	// this formula gives the index of origin of row # i (0-9)
+	int get_row_origin_2(int i) {
+		return (i*9);
+	}
+
+	// get box origin formula
+	// this formula gives the index of origin of column # i (0-8)
+	int get_box_origin_3(int i) {
+		return i;
+	}
+
+	// box step formula
+	// this formula runs through a box shape (i must be less than 9)
+	int box_step_formula(int i) {
+		return boxOrigin + (i/3) * 9 + (i%3);
+	}
+
+	// row step formula
+	int row_step_formula(int i) {
+		return rowOrigin + i;
+	}
+
+	// col step formula
+	int col__step_formula(int i) {
+		return colOrigin + i*9;
+	}
+
+}
+
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
