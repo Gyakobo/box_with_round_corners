d3a70244f39982f8109fbb3e068fcae12d8d2632
Gyakobo <andygyakobo@gmail.com>
2022-07-24 03:21:28 -0400

    Added an install.sh for later use-cases

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-sudo g++ -std=c++11 main.cpp src/*.cpp arcade_games/src/*.cpp src/imgui/imgui.cpp src/imgui/imgui_impl_glfw_gl3.cpp src/imgui/imgui_demo.cpp src/imgui/imgui_draw.cpp -o main -lGL -lGLEW -lglfw3
+sudo g++ -std=c++11 main.cpp src/*.cpp arcade_games/src/*.cpp src/imgui/imgui.cpp src/imgui/imgui_impl_glfw_gl3.cpp src/imgui/imgui_demo.cpp src/imgui/imgui_draw.cpp -o main -lGL -lGLEW -lglfw
 
 sudo ./main
---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libglfw3
+sudo apt-get install libglfw3-dev
+sudo apt-get -y install glew-utils
+sudo apt-get install -y libglew-dev
